3a9b857ddcce225496b7e288b1d141931b55a971
longnphan <67768035+longnphan@users.noreply.github.com>
2020-11-20 23:22:59 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Adds zero in front of minutes and seconds when the time is a single digit character.
+function padTime(time) {
+    return time.toString().padStart(2, "0");
 }
 
-export default App;
+export default function App() {
+    // Timer States
+    const[timeLeft, setTimeLeft] = useState(5 * 60);
+    const[isRunning, setIsRunning] = useState(false);
+
+    // Timer variables
+    const minutes = padTime(Math.floor(timeLeft / 60));
+    const seconds = padTime((timeLeft - (minutes * 60)));
+    const intervalRef = useRef(null);
+
+    // Starts timer
+    function startTimer() {
+
+        setIsRunning(true);
+
+        intervalRef.current = setInterval(() => {
+            setTimeLeft(timeLeft => {
+                if (timeLeft >= 1) {
+                    return timeLeft - 1;
+                } else {
+                    return 0;
+                }
+            });
+        }, 1000);
+    }
+    
+    // Stops Timer
+    function stopTimer() {
+        clearInterval(intervalRef.current);
+
+        setIsRunning(false);
+    }
+
+    // Resets Timer
+    function resetTimer() {
+        clearInterval(intervalRef.current);
+        setTimeLeft(5*60);
+        setIsRunning(false);
+    }
+    
+    return(
+        <div className="container">
+            <h1><u>Timer</u></h1>
+
+            <div className="timer">
+                <span>{minutes}</span>
+                <span>:</span>
+                <span>{seconds}</span>
+            </div>
+
+            <div className="buttons">
+                {/* Conditional rendering that removes the start and reset button if the timer 
+                is running and removes the stop button if the timer is not running */}
+                {!isRunning && <button onClick={startTimer}>Start</button>}
+                <br />
+                {isRunning && <button onClick={stopTimer}>Stop</button>}
+                {!isRunning && <button onClick={resetTimer}>Reset</button>}
+            </div>
+        </div>
+    );
+}
